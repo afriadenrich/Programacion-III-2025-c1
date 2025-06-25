9a0c459221ae506702026ad484505b5de626dd9c
AgustinFrich <agustinfrich@gmail.com>
2025-06-25 19:13:15 -0300

    cors y peticion entre front y back

--- Clase20-Deploy/backend/index.js
+++ Clase20-Deploy/backend/index.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+  })
+);
 
 app.get("/", (req, res) => {
   res.send({ mensaje: "Funciona" });
--- Clase20-Deploy/frontend/main.js
+++ Clase20-Deploy/frontend/main.js
@@ -1,1 +1,7 @@
 console.log("todo anda");
+
+(async () => {
+  const peticion = await fetch("http://localhost:3000");
+  const respuesta = await peticion.json();
+  console.log(respuesta);
+})();
