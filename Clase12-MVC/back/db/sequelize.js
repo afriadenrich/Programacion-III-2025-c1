4794e5d0a5cebdff29dfba74119849515d77acdf
AgustinFrich <agustinfrich@gmail.com>
2025-06-25 19:40:18 -0300

    Cambios para sqlLite

--- Clase12-MVC/back/db/sequelize.js
+++ Clase12-MVC/back/db/sequelize.js
@@ -7,17 +7,24 @@ console.log("Entra a sequelize y lo levanta");
 //   host: "127.0.0.1",
 //   port: 3307,
 // });
+let sequelize;
 
-const sequelize = new Sequelize(
-  "autos_2025_c1",
-  process.env.SQL_USER,
-  process.env.SQL_PASSWORD,
-  {
-    dialect: "mysql",
-    host: process.env.SQL_HOST,
-    port: process.env.SQL_PORT,
-  }
-);
+if (process.env.DIALECTO_DB === "mysql") {
+  sequelize = new Sequelize(
+    "autos_2025_c1",
+    process.env.SQL_USER,
+    process.env.SQL_PASSWORD,
+    {
+      dialect: "mysql",
+      host: process.env.SQL_HOST,
+      port: process.env.SQL_PORT,
+    }
+  );
+} else {
+  sequelize = new Sequelize(process.env.SQLLITE_URI, {
+    dialect: "sqlite",
+  });
+}
 
 //      127.0.0.1             10.74.48.48      15.18.87.57      43.58.54.4      45.3.15.5
 // mi PC / la de mi colega -> desarrollo -> Quality Assurance -> Homologación -> Producción
--- Clase12-MVC/back/index.js
+++ Clase12-MVC/back/index.js
@@ -8,7 +8,7 @@ const bodyParser = require("body-parser");
 
 app.use((req, res, next) => {
   // req.headers["Access-Control-Allow-Origin"] = "http://127.0.0.1:5500";
-  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+  res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN);
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
 
