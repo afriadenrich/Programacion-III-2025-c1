83642e4fe8053b8e7cb05d60a1428814a1ab9d90
AgustinFrich <agustinfrich@gmail.com>
2025-06-25 19:21:55 -0300

    cambios de ambientes

--- Clase20-Deploy/backend/index.js
+++ Clase20-Deploy/backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
 
 app.use(
   cors({
-    origin: "http://127.0.0.1:5500",
+    origin: process.env.ORIGIN,
   })
 );
 
---
+++ Clase20-Deploy/frontend/env.js
@@ -0,0 +1,2 @@
+const API_URL = "https://progra3-backend.vercel.app/";
+// const API_URL = "http://localhost:3000/";
